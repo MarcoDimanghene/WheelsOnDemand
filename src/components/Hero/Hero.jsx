2f15b87a36ac79da0e374344c32c237d28b2be8d
Marco <Dimanghenemarco@gmail.com>
2024-01-03 14:38:02 -0300

    v2 del hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,17 +3,13 @@ import {  HeroContainerStyled, InfoContainerStyled, TextContainerStyled, TitleCo
 
 export const Hero = () => {
     return (
-        <HeroContainerStyled> 
-            <TitleContainerStyled>
-                Wheels On Demand:
-                Explora a Tu Ritmo
-            </TitleContainerStyled>
-            <InfoContainerStyled>
-                
-                <TextContainerStyled>
-                    
-                    <p>¡Bienvenidos a Wheels On Demand! Tu puerta de entrada al emocionante mundo de la exploración sin límites. Con nuestra amplia selección de vehículos de alquiler de primera clase, estamos aquí para llevarte a cualquier destino que desees, a tu propio ritmo. ¿Listo para comenzar tu próxima aventura sobre ruedas? ¡Hazlo con Wheels On Demand!</p>
-                </TextContainerStyled>        
+        
+        <HeroContainerStyled>
+            <InfoContainerStyled> 
+                <TitleContainerStyled>
+                    Wheels On Demand
+                    Explora a Tu Ritmo
+                </TitleContainerStyled>
             </InfoContainerStyled>
         </HeroContainerStyled>
     )
--- src/components/Hero/HeroStyled.js
+++ src/components/Hero/HeroStyled.js
@@ -1,24 +1,44 @@
-import styled from "styled-components";
-import imghero from "../Img/heroimg.jpg"
+import styled, { keyframes } from "styled-components";
+import imghero1 from "./img/001-01.png"
+import imghero2 from "./img/001-02.png"
+import imghero3 from "./img/001-03.png"
+import imghero4 from "./img/001-04.png"
+import imghero5 from "./img/001-05.png"
+import imghero6 from "./img/001-06.png"
+import imghero7 from "./img/001-07.png"
+
+const images = [imghero1, imghero2, imghero3,imghero4,imghero5,imghero6,imghero7];
+
+const changeBackground = keyframes`
+    ${images.map((image, index) => `
+    ${index * 100 / images.length}% {
+        background-image: url(${image});
+    }
+    `)}
+`;
+
+
 
 export const HeroContainerStyled= styled.div `
     max-width: 1400px;
     box-shadow: 0 0 25px #0ef;
     padding:0;
-    background-image: url(${imghero});
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
+    animation: ${changeBackground} 50s  infinite;
 `
 export const TitleContainerStyled = styled.h1`
     color: #fff;
     display:flex;
-    justify-content: center;
-    align-items: center;
-    text-align:center;
-    background-color: #081b29;
-    height: 80px;
-    
+    background-color: rgba(0, 0, 0, 0.7);
+    text-align: center;
+    position: absolute;
+    top: 50px;
+    right: 50px;
+    margin: auto;
+    width:25%;
+    border-radius: 25px;
     @media (max-width: 700px) {
         font-size: 1.5rem;
     }
